1bc4fd5eafa4366f6178d32bb9ac6ad7a97e1397
ranaekiz17 <ekizgrana94@gmail.com>
2023-05-20 16:55:35 +0200

    TestNG 3.GUN

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,31 +3,46 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 import java.util.List;
 
 public class Driver {
-
     static WebDriver driver;
 
     public static WebDriver getDriver() {
-
-
+        String istenenBrowser = ConfigReader.getProperty("browser");
         if (driver == null) {
+            switch (istenenBrowser) {
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "safari":
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+            }
 
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
             driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15L));
         }
 
         return driver;
     }
 
-
     public static void closeDriver() {
         driver.close();
-
+        driver = null;
     }
 }
